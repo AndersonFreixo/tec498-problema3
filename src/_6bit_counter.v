f344c0a3173d422733840d5f027f01211ec4eeea
linux <linux@debian.debian>
2026-06-08 19:31:12 -0300

    add: contador de 6 bits

---
+++ src/_6bit_counter.v
@@ -0,0 +1,16 @@
+module _6bit_counter
+    input  clk,   // Clock principal de entrada
+    input  rst,      // Reset síncrono (ou assíncrono)
+    output reg [5:0] counter,
+	 output done
+);    
+	 
+    always @(posedge clk or posedge rst) begin
+        if (rst) counter <= 6'b000000;
+        else if (counter == 6'b111111) counter = 6'b000000;
+		  else counter += 1;	
+	 end
+	 assign done = (counter == 6'b111111);
+	 
+endmodule
+    
